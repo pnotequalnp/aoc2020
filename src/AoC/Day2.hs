ae40d92b31c8db8eef57d65bc122383d4ccbf3eb
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-12-21 15:11:59 -0800

    Slight day 2 cleanup

--- src/AoC/Day2.hs
+++ src/AoC/Day2.hs
@@ -8,9 +8,7 @@ part1 = pure . T.pack . show . length . filter valid . T.lines
   where
   valid t = x >= l && x <= u
     where
-    (read . T.unpack -> l, T.drop 1 -> t') = T.breakOn "-" t
-    (read . T.unpack -> u, T.drop 1 -> t'') = T.breakOn " " t'
-    (T.head -> c, T.drop 2 -> r) = T.splitAt 1 t''
+    (l, u, c, r) = parse t
     x = T.length . T.filter (==c) $ r
 
 part2 :: Text -> IO Text
@@ -18,6 +16,11 @@ part2 = pure . T.pack . show . length . filter valid . T.lines
   where
   valid t = (r `T.index` (l-1) == c) /= (r `T.index` (u-1) == c)
     where
-    (read . T.unpack -> l, T.drop 1 -> t') = T.breakOn "-" t
-    (read . T.unpack -> u, T.drop 1 -> t'') = T.breakOn " " t'
-    (T.head -> c, T.drop 2 -> r) = T.splitAt 1 t''
+    (l, u, c, r) = parse t
+
+parse :: Text -> (Int, Int, Char, Text)
+parse t = (l, u, c, r)
+  where
+  (read . T.unpack -> l, T.drop 1 -> t') = T.breakOn "-" t
+  (read . T.unpack -> u, T.drop 1 -> t'') = T.breakOn " " t'
+  (T.head -> c, T.drop 2 -> r) = T.splitAt 1 t''
