e3b3659ad6b4714e98380954d027203b1097a43a
Kevin Mullins <46154511+pnotequalnp@users.noreply.github.com>
2020-12-23 14:45:07 -0800

    Slight day 6 cleanup

--- src/AoC/Day6.hs
+++ src/AoC/Day6.hs
@@ -5,8 +5,8 @@ import Data.Text (Text)
 import qualified Data.Text as T
 
 part1 :: Text -> IO Text
-part1 = pure . T.pack . show . sum . fmap (count . T.filter (not . (== '\n'))) . T.splitOn "\n\n"
-  where count = S.size . S.fromList . T.chunksOf 1
+part1 = pure . T.pack . show . sum . fmap count . T.splitOn "\n\n"
+  where count = S.size . S.fromList . T.chunksOf 1 . T.filter (not . (== '\n'))
 
 part2 :: Text -> IO Text
 part2 = pure . T.pack . show . sum . fmap count . T.splitOn "\n\n"
